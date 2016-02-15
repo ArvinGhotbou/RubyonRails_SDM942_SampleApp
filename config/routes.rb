a4bf631f88681afed1989b4e5febd51900bdde94
arving <arvinghotbou@gmail.com>
2016-02-15 15:20:47 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'users/new'
+
+  #get 'static_pages/home'
+  #get 'static_pages/help'
+  # 'static_pages/about'
+  #get 'static_pages/contact'
+  
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'home' => 'static_pages#home'
+  get 'signup' => 'users#new'
   
   #root 'application#hello'
   root 'static_pages#home'
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should get signup" do
+      get :new
+      assert_response :success
+      assert_select "title", "Sign up | #{@base_title}"
+   end
+
+end
