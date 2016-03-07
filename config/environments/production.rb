a4b9be46f8926cbf2d1a3d0c543ed8cb2f82ecda
arving <arvinghotbou@gmail.com>
2016-03-07 13:08:32 +0000

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # MAIL
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'ag-ror-sdm942-sampleapp2.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   
   ActionMailer::Base.smtp_settings = {
